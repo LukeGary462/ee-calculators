57d0ffde5336e8b7d718eae04e85aa454a4ebe62
Luke Gary <sales.reffects@gmail.com>
2018-11-16 15:31:41 -0500

    add summing

--- filters/lpf.py
+++ filters/lpf.py
@@ -7,6 +7,8 @@
 #   @date:                  2018/4/30
 #   @brief:
 #   @verbatim:
+#   @note:                  http://www.ti.com/lit/an/sloa024b/sloa024b.pdf
+#                           http://www.analog.com/en/analog-dialogue/articles/phase-relations-in-active-filters.html
 
 ################################################################
 # @copyright
@@ -25,11 +27,11 @@ import scipy as scy
 import math
 import argparse
 import matplotlib.pyplot as plt
-# from basic_units import secs, hertz, minutes
+import matplotlib.image as mpimg
 
 def get_options():
     parser = argparse.ArgumentParser(description='calculate filter values')
-    parser.add_argument('--type','-t',nargs='?',help='[rc|sallen-key]',default='rc')
+    parser.add_argument('--type','-t',nargs='+',help='[rc|sallen-key]',default=['rc'])
     parser.add_argument('--job','-j',nargs='+',help='[solve|plot]',type=str,default=['plot', 'cap'])
     parser.add_argument('--resistance', '-r', nargs='?', type=float, default=10e3)
     parser.add_argument('--capacitance', '-c', nargs='?', type=float, default=1e-9)
@@ -43,6 +45,13 @@ def map_filter_type_to_solver(type):
     switch = {
         'rc': solve_rc,
         'sallen-key': solve_sallen_key,
+    }
+    return switch.get(type, -1)
+
+def mapp_type_to_img(type):
+    switch = {
+        'rc': 'img/lp-1-rc.png',
+        'sallen-key': 'img/lp-sk.png',
     }
     return switch.get(type, -1)
 
@@ -78,56 +87,87 @@ def plot_rc(Fc, Fm, R, C):
 
     return a, phase, F, fc
 
+def solve_sallen_key(solve, Fc, R, C):
+    # assume R1 = R2, and C1 = C2
+    if solve[0] == 'c':
+        C = np.power(((1.0/Fc) / (2*np.pi)),2)/R
+    else:
+        R = np.power(((1.0/Fc) / (2*np.pi)),2)/C
+    return R, C
 
-def solve_sallen_key(solve, Fc, Fm, R, C):
-    pass
+def plot_sallen_key(Fc, Fm, R, C):
+    F       = np.geomspace(0.1, Fm, dtype=float)
+    Xc      = np.empty_like(F)
+    gain    = np.empty_like(F)
+    phase   = np.empty_like(F)
+    a       = np.empty_like(F)
+    fc      = 1.0 / ((2*np.pi)*( np.sqrt(R*R*C*C) ) )
+    Q       = np.sqrt(R*R*C*C) / (C*2*R)
+    alpha   = ((fc*2*np.pi)/Q)/2
+    #solve gain
+    for i in range(0, len(F)):
+        #cap reactance
+        Xc[i] = 1 / (2*np.pi*F[i]*C)
+        #gain
+        numerator = Xc[i] * Xc[i]
+        denomenator = (R+R) + (Xc[i]*(R+R)) + (Xc[i]*Xc[i])
+        a[i] = numerator / denomenator
+        p = -1.0*np.arctan((1/alpha)*((2*2*np.pi*F[i]/fc)+(np.sqrt(4-np.power(alpha,2)))))
+        phase[i] = p - p
 
-def plot_sallen_key(solve, Fc, Fm, R, C):
-    pass
+    return a, phase, F, fc
 
 
 
 if __name__ == "__main__":
     OPTIONS = get_options()
 
-    cutoff = OPTIONS.frequency_cut
-    maxFrequency = OPTIONS.frequency_max
-    res = OPTIONS.resistance
-    cap = OPTIONS.capacitance
+    plt.figure()
+    for archType in OPTIONS.type:
+        cutoff = OPTIONS.frequency_cut
+        maxFrequency = OPTIONS.frequency_max
+        res = OPTIONS.resistance
+        cap = OPTIONS.capacitance
 
-    if OPTIONS.job[0] == 'solve':
-        solver = map_filter_type_to_solver(OPTIONS.type)
-        res, cap = solver(OPTIONS.job[1], cutoff, res, cap)
+        if OPTIONS.job[0] == 'solve':
+            solver = map_filter_type_to_solver(archType)
+            res, cap = solver(OPTIONS.job[1], cutoff, res, cap)
 
-    print(f'SOLVE R: {res} Ohms')
-    print(f'SOLVE C: {cap} Farads')
-    plotter = map_filter_type_to_plotter(OPTIONS.type)
-    gain, phase, frequencies, fc = plotter(cutoff, maxFrequency, res, cap)
+        print(f'SOLVE R: {res} Ohms')
+        print(f'SOLVE C: {cap} Farads')
+        plotter = map_filter_type_to_plotter(archType)
+        gain, phase, frequencies, fc = plotter(cutoff, maxFrequency, res, cap)
 
-    print(f'f-3dB:{fc} Hz')
-    print(f'GAIN:{gain} 1.0/1.0')
-    print(f'PHASE:{phase}')
+        print(f'f-3dB:{fc} Hz')
+        print(f'GAIN:{gain} 1.0/1.0')
+        print(f'PHASE:{phase}')
 
+
+        if OPTIONS.plot == True:
+            plt.subplot(221)
+            img = mpimg.imread(mapp_type_to_img(archType))
+            plt.imshow(img)
+
+            plt.suptitle(f'{archType} Filter, [{cutoff}Hz : 0Hz-{maxFrequency}Hz], [{res}Ohms:{cap}Farads]')
+            plt.subplot(222)
+            plt.title('Gain')
+            if OPTIONS.loglog == True:
+                plt.loglog(frequencies, gain, label=archType)
+            else:
+                plt.semilogx(frequencies, gain, label=archType)
+            plt.axvline(fc, color='r')
+            plt.grid(True, which='major')
+            plt.grid(True, which='minor')
+            plt.minorticks_on()
+
+            plt.subplot(224)
+            plt.title('Phase')
+            plt.semilogx(frequencies, phase, label=archType)
+            plt.axvline(fc, color='r')
+            plt.grid(True, which='major')
+            plt.grid(True, which='minor')
+            plt.minorticks_on()
 
     if OPTIONS.plot == True:
-        plt.suptitle(f'{OPTIONS.type} Filter, [{cutoff}Hz : 0Hz-{maxFrequency}Hz], [{res}Ohms:{cap}Farads]')
-        plt.subplot(211)
-        plt.title('Gain')
-        if OPTIONS.loglog == True:
-            plt.loglog(frequencies, gain)
-        else:
-            plt.semilogx(frequencies, gain)
-        plt.axvline(fc, color='r')
-        plt.grid(True, which='major')
-        plt.grid(True, which='minor')
-        plt.minorticks_on()
-
-        plt.subplot(212)
-        plt.title('Phase')
-        plt.semilogx(frequencies, phase)
-        plt.axvline(fc, color='r')
-        plt.grid(True, which='major')
-        plt.grid(True, which='minor')
-        plt.minorticks_on()
-
+        plt.legend(loc='best')
         plt.show()
